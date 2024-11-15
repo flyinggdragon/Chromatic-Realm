73cba29c7c4a1b9a689f12e67d2be4e024cf1bf7
gairin <marco14963@gmail.com>
2024-11-15 14:19:46 -0300

    FIX: Parallax

--- Assets/Scripts/Parallax.cs
+++ Assets/Scripts/Parallax.cs
@@ -1,27 +1,27 @@
 using UnityEngine;
 
 public class Parallax : MonoBehaviour {
-    private float startPos, length;
+    private float startPosX, length;
     public GameObject cam;
     public float parallaxEffect;
 
     private void Start() {
-        startPos = transform.position.x;
+        startPosX = transform.position.x;
         length = GetComponent<SpriteRenderer>().bounds.size.x;
     }
 
     private void FixedUpdate() {
-        float distance = cam.transform.position.x * parallaxEffect;
-        float movement = cam.transform.position.x * (1 * parallaxEffect);
+        float distanceX = cam.transform.position.x * parallaxEffect;
+        float movementX = cam.transform.position.x * (1 - parallaxEffect);
 
-        transform.position = new Vector3(startPos + distance, transform.position.y, transform.position.z);
+        // Atualiza a posição do fundo
+        transform.position = new Vector3(startPosX + distanceX, cam.transform.position.y -4, transform.position.z);
 
-        if (movement > startPos + length) {
-            startPos += length;
-        }
-
-        else if (movement < startPos - length) {
-            startPos -= length;
+        // Lógica de repetição infinita no eixo X
+        if (movementX > startPosX + length) {
+            startPosX += length;
+        } else if (movementX < startPosX - length) {
+            startPosX -= length;
         }
     }
 }
